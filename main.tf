aafa253ecf520cf2c65157345fc231d044914b09
Charles Bushong <charles.bushong@cms.hhs.gov>
2023-01-26 14:48:58 -0500

    BATIAI-1137: Refactor for ADOs (#6)
    * BATIAI-1137: Initial refactor work
    
    * BATIAI-1137: Running pre-commit -a
    
    * BATIAI-1137: Adding a bunch of new variables to support configuring more parameters
    
    * BATIAI-1137: Force publicly_accessible to false
    
    * BATIAI-1137: Adding 'try' to fix issues when hosted zone is provided

--- main.tf
+++ main.tf
@@ -1,17 +1,23 @@
-
+locals {
+  engine_version_short = split(".", var.engine_version)[0]
+}
 module "aurora" {
   source  = "terraform-aws-modules/rds-aurora/aws"
   version = "6.1.4"
 
   name           = var.name
   engine         = "aurora-postgresql"
-  engine_version = "13.4"
-  instances = {
-    1 = {
-      instance_class      = "db.r5.xlarge"
-      publicly_accessible = var.publicly_accessible
-    }
-  }
+  engine_version = var.engine_version
+
+  instances = { for index in range(var.instance_count) : index + 1 =>
+    merge(
+      {},
+      var.instance_identifier_pseudoprefix != "" ? { identifier = "${var.instance_identifier_pseudoprefix}${index + 1}" } : {}
+  ) }
+  publicly_accessible = false
+  instance_class      = var.instance_class
+
+  deletion_protection = var.deletion_protection
 
   endpoints = {
     static = {
@@ -22,7 +28,8 @@ module "aurora" {
 
   vpc_id                  = var.vpc_id
   subnets                 = var.subnets
-  create_db_subnet_group  = true
+  create_db_subnet_group  = var.create_db_subnet_group
+  db_subnet_group_name    = var.subnet_group_name
   create_security_group   = true
   allowed_security_groups = var.allowed_security_groups
   security_group_egress_rules = {
@@ -34,11 +41,12 @@ module "aurora" {
 
   iam_database_authentication_enabled = true
   master_username                     = var.master_username
-  create_random_password              = true
+  create_random_password              = var.create_random_password
   database_name                       = var.database_name
+  backup_retention_period             = var.backup_retention_period
 
   apply_immediately   = true
-  skip_final_snapshot = false
+  skip_final_snapshot = var.skip_final_snapshot
 
   db_parameter_group_name         = aws_db_parameter_group.db_parameter_group.id
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.db_cluster_parameter_group.id
@@ -48,34 +56,40 @@ module "aurora" {
 }
 
 resource "aws_db_parameter_group" "db_parameter_group" {
-  name        = "${var.name}-aurora-db-postgres13-parameter-group"
-  family      = "aurora-postgresql13"
-  description = "${var.name}-aurora-db-postgres13-parameter-group"
+  name        = "${var.name}-aurora-db-postgres${local.engine_version_short}-parameter-group"
+  family      = "aurora-postgresql${local.engine_version_short}"
+  description = "${var.name}-aurora-db-postgres${local.engine_version_short}-parameter-group"
   dynamic "parameter" {
     for_each = var.db_parameter_group_parameters
     content {
-      name = parameter.value["name"]
+      name  = parameter.value["name"]
       value = parameter.value["value"]
     }
   }
-  tags        = var.tags
+  tags = var.tags
 }
 resource "aws_rds_cluster_parameter_group" "db_cluster_parameter_group" {
-  name        = "${var.name}-aurora-postgres13-cluster-parameter-group"
-  family      = "aurora-postgresql13"
-  description = "${var.name}-aurora-postgres13-cluster-parameter-group"
+  name        = "${var.name}-aurora-postgres${local.engine_version_short}-cluster-parameter-group"
+  family      = "aurora-postgresql${local.engine_version_short}"
+  description = "${var.name}-aurora-postgres${local.engine_version_short}-cluster-parameter-group"
   dynamic "parameter" {
     for_each = var.db_cluster_parameter_group_parameters
     content {
-      name = parameter.value["name"]
+      name  = parameter.value["name"]
       value = parameter.value["value"]
     }
   }
-  tags        = var.tags
+  tags = var.tags
+}
+
+data "aws_route53_zone" "cms_zone" {
+  count        = var.route53_zone_base_domain != "" ? 1 : 0
+  name         = var.route53_zone_base_domain
+  private_zone = true
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = var.route53_zone_id
+  zone_id = coalesce(var.route53_zone_id, try(data.aws_route53_zone.cms_zone[0].zone_id,""))
   name    = var.route53_record_name
   type    = "CNAME"
   ttl     = "60"
--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "name" {}
-variable "engine_version" {}
-variable "publicly_accessible" {
-  default = "false"
+variable "engine_version" {
+  default = "13.4"
 }
 
 variable "vpc_id" {}
@@ -18,13 +17,22 @@ variable "master_username" {}
 variable "database_name" {}
 
 variable "tags" {
+  type = map(string)
   default = {
     Owner = "Batcave"
   }
 }
 
-variable "route53_zone_id" {}
-variable "route53_record_name" {}
+variable "route53_zone_id" {
+  default = ""
+}
+variable "route53_zone_base_domain" {
+  description = "If route53_zone_id is an empty string, this variable is used to lookup the r53 zone dynamicaly"
+  default     = ""
+}
+
+variable "route53_record_name" {
+}
 
 variable "worker_security_group_id" {}
 variable "cluster_security_group_id" {}
@@ -37,31 +45,94 @@ variable "db_parameter_group_parameters" {
     {
       name  = "log_destination"
       value = "csvlog"
-    }, {
+      }, {
       name  = "log_connections"
       value = "1"
-    }, {
+      }, {
       name  = "log_disconnections"
       value = "1"
-    }, {
-        name  = "log_statement"
+      }, {
+      name  = "log_statement"
       value = "mod"
-    }, {
+      }, {
       name  = "rds.force_admin_logging_level"
       value = "info"
-    }, {
+      }, {
       name  = "pgaudit.log"
       value = "ddl, role, write"
     }
   ]
 }
 
+
 variable "db_cluster_parameter_group_parameters" {
   type = list(map(string))
 
   default = [{
-      name  = "rds.force_autovacuum_logging_level"
-      value = "warning"
+    name  = "rds.force_autovacuum_logging_level"
+    value = "warning"
     }
   ]
+}
+
+variable "instance_class" {
+  default     = "db.r5.xlarge"
+  description = "Instance classes for instances created under the cluster"
+}
+
+variable "instance_count" {
+  default     = 1
+  description = "How many instances to create under the cluster"
+}
+
+variable "subnet_group_name" {
+  type        = string
+  default     = null
+  description = "Subnet group name, overriding the default of cluster_name"
+}
+variable "create_db_subnet_group" {
+  type    = bool
+  default = true
+}
+
+variable "instance_identifier_pseudoprefix" {
+  type        = string
+  default     = ""
+  description = "A string prefix for the database instance names where the instance number will be appended. This is not to be confused with the 'name_prefix' field.  In general, leave this empty unless you're importing existing resources"
+}
+
+variable "deletion_protection" {
+  type    = bool
+  default = false
+}
+
+variable "maintenance_window" {
+  type    = string
+  default = "mon:05:00-mon:06:00"
+}
+
+variable "database_users" {
+  description = "A list of maps with user_name and secrets_manager_name to trigger the creation of aws secretsmanager secrets"
+  type = list(object({
+    user_name            = string
+    secrets_manager_name = string
+  }))
+  default = []
+}
+
+variable "create_random_password" {
+  description = "Determines whether to create random password for RDS primary cluster"
+  type        = bool
+  default     = true
+}
+
+variable "backup_retention_period" {
+  type        = number
+  default     = 7
+  description = "The days to retain backups for. Default 7"
+}
+
+variable "skip_final_snapshot" {
+  type    = bool
+  default = false
 }
